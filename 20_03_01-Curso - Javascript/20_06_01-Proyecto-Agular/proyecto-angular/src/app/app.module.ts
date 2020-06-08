58eb7e059b3f37916380d7f79870eacbfe54e6d9
Francisco Javier Martinez Duran <user.streaming.001@gmail.com>
2020-06-07 23:24:16 -0500

    Portafolio

--- 20_03_01-Curso - Javascript/20_06_01-Proyecto-Agular/proyecto-angular/src/app/app.module.ts
+++ 20_03_01-Curso - Javascript/20_06_01-Proyecto-Agular/proyecto-angular/src/app/app.module.ts
@@ -1,14 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { routing, appRoutingProviders } from './app.routing';
 
 import { AppComponent } from './app.component';
+import { AboutComponent } from './components/about/about.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { CreateComponent } from './components/create/create.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { ErrorComponent } from './components/error/error.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    ProjectsComponent,
+    CreateComponent,
+    ContactComponent,
+    ErrorComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    routing,
+    appRoutingProviders
   ],
   providers: [],
   bootstrap: [AppComponent]
