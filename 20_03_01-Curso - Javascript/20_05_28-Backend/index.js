abb6d1f16874b3bd2fa3a7695e1614a3daf1c46c
Francisco Javier Martinez Duran <user.streaming.001@gmail.com>
2020-06-14 18:59:21 -0500

    Portafolio

--- 20_03_01-Curso - Javascript/20_05_28-Backend/index.js
+++ 20_03_01-Curso - Javascript/20_05_28-Backend/index.js
@@ -7,11 +7,11 @@ var port = 3700;
 mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost:27017/portafolio')
     .then(() => {
-        console.log("Conexion a la base de datos establecida con satisfactoriamente...");
+        console.log("Conexion a la base de datos establecida satisfactoriamente...");
 
         // Creacion del servidor
-        app.listen(port, () =>{
+        app.listen(port, () => {
             console.log("Servidor corriendo correctamente en la url: localhost:3700");
-        })
+        });
     })
     .catch(err => console.log(err));
