057bf7608616c41d257072af39b6a1648291380f
Francisco Javier Martinez Duran <user.streaming.001@gmail.com>
2020-06-28 21:00:06 -0500

    Aplicacion Chat

--- 20_03_01-Curso - Javascript/20_06_27-curso-sockets/curso-sockets/server/index.js
+++ 20_03_01-Curso - Javascript/20_06_27-curso-sockets/curso-sockets/server/index.js
@@ -3,14 +3,26 @@ var app = express();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
 
-app.use(express.static('client'))
+app.use(express.static('client'));
 
 app.get('/hola-mundo', function(req, res) {
     res.status(200).send('Hola mundo desde una ruta');
 });
 
+var messages = [{
+    id: 1,
+    text: 'Bienvenido al Chat Privado de Socket.io y NodeJS...',
+    nickname: 'Bot - Frank'
+}];
+
 io.on('connection', function(socket) {
-    console.log("El Nodo con IP: " + socket.handshake.address + "se ha conectado...");
+    console.log("El Nodo con IP: " + socket.handshake.address + " se ha conectado...");
+
+    socket.emit('messages', messages);
+    socket.on('add-message', function(data) {
+        messages.push(data);
+        io.sockets.emit('messages', messages);
+    });
 });
 
 server.listen(6677, function() {
