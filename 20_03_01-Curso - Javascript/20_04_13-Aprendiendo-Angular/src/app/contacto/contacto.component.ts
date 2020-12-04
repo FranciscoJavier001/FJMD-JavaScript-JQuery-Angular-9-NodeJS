6636f99a775ab2178a53188e339243e56e6eb8b8
Francisco Javier Martinez Duran <user.streaming.001@gmail.com>
2020-12-03 23:55:40 -0600

    Documentacion

--- 20_03_01-Curso - Javascript/20_04_13-Aprendiendo-Angular/src/app/contacto/contacto.component.ts
+++ 20_03_01-Curso - Javascript/20_04_13-Aprendiendo-Angular/src/app/contacto/contacto.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ContactoUsuario } from '../models/contacto.usuario';
+import { AboutComponent } from '../../../../20_06_01-Proyecto-Agular/proyecto-angular/src/app/components/about/about.component';
 
-@Component({
+@Component({ //** estos son los componentes con los selectores,. l emplate url y el style */
   selector: 'app-contacto',
   templateUrl: './contacto.component.html',
   styleUrls: ['./contacto.component.css']
